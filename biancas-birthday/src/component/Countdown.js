6c2a5e12969f3c0d8b62a2c0f186b431d9dd7e86
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-06-13 14:37:20 +0100

    Added useState consts, useEffect , setInterval

--- biancas-birthday/src/component/Countdown.js
+++ biancas-birthday/src/component/Countdown.js
@@ -1,9 +1,54 @@
 import React, {useState, useEffect} from 'react'
 
-const Countdown = () => {
-    
+const Countdown = ({birthday}) => {
+ const [days, setDays] = useState(0)
+ const [hours, setHours] = useState(0)
+ const [minutes, setMinutes] = useState(0)
+ const [seconds, setSeconds] = useState(0)
+
+ useEffect(() => {
+    const timer = setInterval(() => {
+      const today = new Date().getTime()
+      const timeLeft = birthday - today
+      console.log(timeLeft);
+
+      let day = 24*60*60*1000
+      let hour = 60*60*1000
+      let minute = 60*1000
+      let second = 1000
+
+      let days = Math.floor(timeLeft / day)
+      let hours = Math.floor(timeLeft / hour)
+      let minutes = Math.floor((timeLeft % hour) / minute)
+      let seconds = Math.floor((timeLeft % minute) / second) 
+
+      setDays(days)
+      setHours(hours)
+      setMinutes(minutes)
+      setSeconds(seconds)
+    },1000)
+    return () => clearInterval(timer)
+ },[birthday])
+
   return (
-    <div>Countdown</div>
+    <div className='flex items-center'>
+        <div className='flex flex-col items-center'>
+            <h3>Days</h3>
+            <p>{days}</p>
+        </div>
+        <div className='flex flex-col items-center'>
+            <h3>Hours</h3>
+            <p>{hours}</p>
+        </div>
+        <div className='flex flex-col items-center'> 
+            <h3>Minutes</h3>
+            <p>{minutes}</p>
+        </div>
+        <div className='flex flex-col items-center'>
+            <h3>Seconds</h3>
+            <p>{seconds}</p>
+        </div>
+    </div>
   )
 }
 
