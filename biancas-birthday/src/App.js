965c859cbd2bcbe3ddba43eb7859fc9084476103
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-06-13 14:02:12 +0100

    Add component folder and countdown.js. Added to App.js

--- biancas-birthday/src/App.js
+++ biancas-birthday/src/App.js
@@ -1,3 +1,4 @@
+import Countdown from "./component/Countdown";
 
 function App() {
  const birthday = new Date("Jun 14, 2023 10:15").getTime()
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="flex flex-col justify-center items-center text-orange-400 font-bold w-full h-screen bg-pink-200">
      <h2>Bianca's Birthday</h2>
+     <Countdown birthday={birthday} />
     </div>
   );
 }
---
+++ biancas-birthday/src/component/Countdown.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+
+const Countdown = () => {
+    
+  return (
+    <div>Countdown</div>
+  )
+}
+
+export default Countdown
