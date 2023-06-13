5de3bdd92eb347bd9327553c1a357c24e028f521
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-06-13 13:39:46 +0100

    h2 title added to App.js

--- biancas-birthday/src/App.js
+++ biancas-birthday/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
 
 function App() {
+ const birthday = new Date("Jun 14, 2023 10:15").getTime()
+ console.log(birthday);
+
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="flex flex-col justify-center items-center text-orange-400 font-bold w-full h-screen bg-pink-200">
+     <h2>Bianca's Birthday</h2>
     </div>
   );
 }
